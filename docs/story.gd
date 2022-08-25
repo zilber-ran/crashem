f95838b3670466e26184721c946e2bfdf4ad2988
Ran Zilber <zilber.ran@gmail.com>
2022-08-25 15:11:52 +0300

    story added

---
+++ docs/story.gd
@@ -0,0 +1,37 @@
+"""
+-===========-
+Game Features
+-===========-
+==
+Game Cycle Mechanics
+==
+- SinkHole 
+- CrawlHole
+- NightMode
+- Bosses
+
+==
+Release(s) BackLog
+==
+- BasicBoard (Shape and design)
+- Enemy Types
+- Perks (Topic)!!!!!!!!
+- Tower
+- GameDifficulties
+
+
+==
+Minimal Release
+==
+()[R] Tower
+(/)[R] BasicBoard
+()[M] BasicEnemy
+()[M] BasicPlayer 
+()[R] ExtraBalls-Perk
+()[M] EscalatingDifficulty
+()[R] BasicUI
+()[R] BasicMenues
+()[M] Sounds
+()[R] Release
+
+"""
