93fbc1bb195aeaad58cadce7ff92f54a1b541262
Ran Zilber <zilber.ran@gmail.com>
2022-08-25 15:13:49 +0300

    Adding ball factory

---
+++ Scenes/BallFactory.gd
@@ -0,0 +1,19 @@
+extends SpawnFactory
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func _ready():
+	load_scene("Ball")
+
+
+
+func _input(event):
+	if event.is_action_pressed("ui_select"):
+		print("space pressed")
+		spawn()
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
