ebc876a4b9cc2004da3a30056dab431af91975b5
Ran Zilber <zilber.ran@gmail.com>
2022-08-25 14:25:42 +0300

    Adding Spawn Factory

---
+++ Scenes/Main.gd
@@ -0,0 +1,34 @@
+class_name SpawnFactory
+extends Node2D
+
+var scene = null
+
+## support for property editor (optional)
+export(PackedScene) var scene_res_path
+
+func load_scene(scene_name):
+	"""
+	fast loading implementation. 
+	having big scen(s) we might need to swich using 
+		ResoceLoader.load()
+	"""
+	if not scene_res_path:
+		scene_res_path = 'res://Scenes/'+scene_name+'.tscn'
+	scene = load(scene_res_path)
+	
+func is_loaded():
+	return scene != null
+	 
+func spawn() -> void:
+	if is_loaded():
+		var child = scene.instance()
+		add_child(child)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Scripts/Enemy_Factory.gd
+++ Scripts/Enemy_Factory.gd
@@ -1,23 +1,19 @@
-extends Node2D
+extends SpawnFactory
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var enemy_scene = load("res://Scenes/Enemy.tscn")
-
-func _summon_enemy() -> void:
-	var enemy = enemy_scene.instance()
-	add_child(enemy)
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	load_scene("Enemy")
+
+
 
 func _input(event):
 	if event.is_action_pressed("ui_select"):
 		print("space pressed")
-		_summon_enemy()
+		spawn()
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
