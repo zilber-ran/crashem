eb40da7768cc2771ac7677b17354eaef6aec1739
Ran Zilber <zilber.ran@gmail.com>
2022-08-26 21:59:28 +0300

    moving scripts in order

--- Scripts/Player(new) RB2D.gd
+++ Scripts/Player(new) RB2D.gd
@@ -1,20 +1,29 @@
-extends RigidBody2D
+extends Control
 
-const Mouse_State = {State_Clicked = 0, State_Held = 1, State_Released = 2, State_Neutral=3}
 var pos1 = Vector2.ZERO
 var pos2 = Vector2.ZERO
 
-func _input(event):
-	match(Mouse_State.key):
-		0:
-			pos1 = get_global_mouse_position()
-		1:
-			pos2 = get_global_mouse_position()
-		2:
-			var thrust = Vector2(pos1-pos2).length()
-			_launch(thrust)
-		3:
-			pass
-			
+func _notification(what):
+	match what:
+		NOTIFICATION_MOUSE_ENTER:
+			pass # Mouse entered the area of this control.
+		NOTIFICATION_MOUSE_EXIT:
+			pass # Mouse exited the area of this control.
+		NOTIFICATION_FOCUS_ENTER:
+			pass # Control gained focus.
+		NOTIFICATION_FOCUS_EXIT:
+			pass # Control lost focus.
+		NOTIFICATION_THEME_CHANGED:
+			pass # Theme used to draw the control changed;
+			# update and redraw is recommended if using a theme.
+		NOTIFICATION_VISIBILITY_CHANGED:
+			pass # Control became visible/invisible;
+			# check new status with is_visible().
+		NOTIFICATION_RESIZED:
+			pass # Control changed size; check new size
+			# with get_size().
+		NOTIFICATION_MODAL_CLOSE:
+			pass # For modal pop-ups, notification
+			# that the pop-up was closed.
 func _launch(Thrust):
 	pass
