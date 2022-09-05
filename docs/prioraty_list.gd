b9d24c49a92e48549e6130055fbe25050eeaefa3
Maayooky <zilber.maayan@gmail.com>
2022-09-05 16:25:18 +0300

    Made Priority list & Game Ideas
    In order to clear out everything gameplay related I added a list of must-have features and have written some ideas that implement them...

---
+++ docs/prioraty_list.gd
@@ -0,0 +1,87 @@
+""""
+======================================
+	GAMEPLAY FEATURES PRIORATY LIST:
+======================================
+
+1.Launching a Ball that bumps and bounces around the map
+
+2.Enemies to squish with the ball - gradually getting more difficult
+
+3.some sort of goal as the player - can be highest score or another reachable goal
+
+4.upgradeable charachter - making the game easier as you progress(Rougelite)
+
+==========
+Concepts:
+==========
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+1.Billiard/pool style game
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+**Game Goal: Reach higher levels on each run
+
+**Game Genre: #Strategy #TurnBased #Rougelite #TowerDeffense game
+	
+**Map Scale: Small/Medium
+	
+**Description:
+	-the player controlls the ball and launches it towards incoming enemies to deffend
+the tower, which poses as the players health. When an enemy reaches the tower it 
+attacks it and lowers it's HP. When the tower's HP drops to 0 the game ends with
+the player losing.
+
+The game works on a turn based system, on the player's turn they luanch themselves 
+towards the enemies. Enemies hit will lose health or die. After the player stoped moving
+it becomes the enemies' turn: moving, spawning new enemies and attacking the tower.
+Enemies will grdually get more difficult as the game progresses.
+
+On higher levels of progress the player gets access to more balls
+enabling it to use them to attack more enemies per turn.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+2.Tactical Tower deffense style game
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+**Game Goal: COMPLETE OBJECTIVES to Reach higher levels on each run
+
+**Game Genre: #Strategy #Action #Rougelite #TowerDeffense game
+	
+**Map Scale: Large/Huge
+
+**Description:
+	-the player controlls the ball and launches it towards incoming enemies to deffend
+the tower, which poses as the players health. When an enemy reaches the tower it 
+attacks it and lowers it's HP. When the tower's HP drops to 0 the game ends with
+the player losing.
+
+The game runs constently, with a set objective for every phase of the game. The player
+might be tasked with protecting the tower form a wave of incoming enemies, or covering 
+the tower and sheilding form enemies as it moves from point A to B. Another task might
+be attacking enemy bases, with no concern for the tower at that -
+-phase{bonus lvl/diffrent goal}
+
+The player attacks by laucning itself towards the enemies and squishing them in the 
+process. Enemies will grdually get more difficult as the game progresses. At higher
+levels of progress the player will have upgraded stats(2B astablished)
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+3.Arcade Style game
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+**Game Goal: CLEAR LEVELS to REACH HIGHEST SCORE on each run
+
+**Game Genre: #Arcade #Action #Rougelite #Strategy
+
+**Map Scale: Ranging from Small to Large depending on the current level
+
+*Description:
+	-The player is the balll, on each level of the game it must defeat all the
+enemies to pass to the next one. the player defeats enemies by launching itself at
+them causing it to crash'em. 
+Enemies will not harm the player, it's the oppesite actually. Enemies will have an 
+awerness level telling them how much they can sense the player{like:Sleep,wander,flee}.
+On each level the player must hit all the enemies within a set amount of launches,
+failing to do so will end the game with the player losing.
+As the game progresses enemies will get tougher, and the player will aquire various
+upgrades making their job easier...
+
+
+"""
