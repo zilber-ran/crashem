b10c99da28d32b7ef69d9d77ceddd0d05f45ffce
Maayooky <zilber.maayan@gmail.com>
2022-09-13 13:51:24 +0300

    refining game context
    **should think about using functions instead of dictionaries for LVL constants...
    less tedious
    o((⊙﹏⊙))o.

--- Scripts/game_context.gd
+++ Scripts/game_context.gd
@@ -15,11 +15,19 @@ var ENEMY_TYPES_PER_LEVEL  = {
 
 var DIFFICULTY_INCREASE_PER_LEVEL = {
 	1: false , 2: true , 3: false ,
-	4: false 
+	4: false
 }
 
-var ENEMY_CLUSTER_SIZE_PER_LEVEL = {
+export var ENEMY_CLUSTER_SIZE_PER_LEVEL =  {
 	1: 2, 2: 2, 3: 3
+}
+
+var PLAYER_LAUNCHES_PER_LEVEL = {
+	1: 5, 2:5, 3:4
+}
+
+var TOTAL_CRAWLER_HOLES_PER_LEVEL = {
+	1:2, 2:2, 3:1, 4:2
 }
 
 export var difficulty : int = 1
@@ -45,8 +53,8 @@ func next_game_level():
 
 func load_level() ->void:
 	calculate_player_launches()
-	manage_crawler_holes()
-	activate_crawler_holes()
+#	manage_crawler_holes()
+#	activate_crawler_holes()
 
 func next_game_difficulty():
 	difficulty += 1
@@ -74,10 +82,19 @@ func get_crawler_holes_spawn_radius():
 	return 50
 	
 func calculate_player_launches() -> void:
-	pass
+	var remaning_launches = max_player_launches-current_player_launches
+	max_player_launches = PLAYER_LAUNCHES_PER_LEVEL[level]
+	if remaning_launches >> 0:
+		max_player_launches += remaning_launches
 
-func manage_crawler_holes() -> void:
-	pass
 
-func activate_crawler_holes() -> void:
-	pass
+#func manage_crawler_holes() -> void:
+#	var node2d = Node2D.new()
+#	var crawler_holes =  node2d.get_tree().get_nodes_in_group("CrawlerHole")
+#	if crawler_holes.get_node_count() == get_total_crawler_holes():
+#		return
+"Called by factory	 (～￣▽￣)～	"
+
+#func activate_crawler_holes() -> void:
+#	pass
+"Called by factory	 ＼（〇_ｏ）／	"
