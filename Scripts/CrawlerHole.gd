4b7c49eed2de9db327292f864d4da9a37b44a851
Maayooky <zilber.maayan@gmail.com>
2022-09-13 19:18:28 +0300

    Added Spawning bounderies
    Enemies and Scrawl_Holes now apeer only within game borders

--- Scripts/CrawlerHole.gd
+++ Scripts/CrawlerHole.gd
@@ -10,6 +10,8 @@ export var difficulty : int = ctx.difficulty
 export var cluster_size :int = ctx.get_enemy_cluster_size()
 export var player_position :Vector2 = Vector2.ZERO
 
+onready var board_size = ctx.board_size
+
 func is_positioned():
 	return position != Vector2.ZERO
 
@@ -20,9 +22,13 @@ func random_position(center):
 	"""
 	sets  a random position
 	"""
-	position = center + vd.distort_vector2_Wlength(pos_radius)
-	logger.log_debug(logger.STATUS_PASSED, logger.format("CrollerHole random pos: {}", [position]))
-	
+	var estimeted_pos = center + vd.distort_vector2_Wlength(pos_radius)
+	if -board_size < estimeted_pos.x && estimeted_pos.x < board_size && \
+	-board_size < estimeted_pos.y && estimeted_pos.y < board_size:
+		position = estimeted_pos
+		logger.log_debug(logger.STATUS_PASSED, logger.format("CrollerHole random pos: {}", [position]))
+		return
+	else: random_position(center)
 
 func spawnem():
 	var cluster_size = ctx.get_enemy_cluster_size()
@@ -34,10 +40,13 @@ func spawnem():
 	while counter>0:
 		logger.log_debug(logger.STATUS_PASSED, logger.format("CrollerHole enemy creation {} of {}", [cluster_size-counter+1, cluster_size]))
 		for type in types:
-			enemy = load_and_spawn(type)
-			enemy.position = vd.distort_vector2_Wlength(spawn_radius)
-			enemy._on_setup_called(difficulty)
-		counter -= 1
+			var estimated_pos = vd.distort_vector2_Wlength(spawn_radius)
+			if -board_size < estimated_pos.x && estimated_pos.x < board_size && \
+	-board_size < estimated_pos.y && estimated_pos.y < board_size:
+				enemy = load_and_spawn(type)
+				enemy.position = estimated_pos
+				enemy._on_setup_called(difficulty)
+				counter -= 1
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
