e1e5a31b69caccf18d36a94b41df34a8f2bdf454
Ran <zilber.ran@gmail.com>
2022-10-14 12:06:51 +0300

    Added GreenWizard

--- Scripts/game_context.gd
+++ Scripts/game_context.gd
@@ -66,7 +66,7 @@ func get_enemy_groups():
 	
 func get_enemy_types():
 #	return ENEMY_TYPES_PER_DIFICULTY.get(difficulty, ["NA"])
-	return ["RedWizard", "BlueWizard"]
+	return ["RedWizard", "BlueWizard", "GreenWizard"]
 	
 func _should_incr_difficulty():
 	return (level%2) > 0
